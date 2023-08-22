51a36f74b865026ce7a626e5b94fbc8f391643da
Daveshammer <daidadalaile@gmail.com>
2023-08-23 00:18:42 +0800

    first commit

---
+++ build/CMakeFiles/mymuduo.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/djf/sourcecode/mymuduo/Acceptor.cc" "CMakeFiles/mymuduo.dir/Acceptor.o" "gcc" "CMakeFiles/mymuduo.dir/Acceptor.o.d"
+  "/home/djf/sourcecode/mymuduo/Buffer.cc" "CMakeFiles/mymuduo.dir/Buffer.o" "gcc" "CMakeFiles/mymuduo.dir/Buffer.o.d"
+  "/home/djf/sourcecode/mymuduo/Channel.cc" "CMakeFiles/mymuduo.dir/Channel.o" "gcc" "CMakeFiles/mymuduo.dir/Channel.o.d"
+  "/home/djf/sourcecode/mymuduo/CurrentThread.cc" "CMakeFiles/mymuduo.dir/CurrentThread.o" "gcc" "CMakeFiles/mymuduo.dir/CurrentThread.o.d"
+  "/home/djf/sourcecode/mymuduo/DefaultPoller.cc" "CMakeFiles/mymuduo.dir/DefaultPoller.o" "gcc" "CMakeFiles/mymuduo.dir/DefaultPoller.o.d"
+  "/home/djf/sourcecode/mymuduo/EPollPoller.cc" "CMakeFiles/mymuduo.dir/EPollPoller.o" "gcc" "CMakeFiles/mymuduo.dir/EPollPoller.o.d"
+  "/home/djf/sourcecode/mymuduo/EventLoop.cc" "CMakeFiles/mymuduo.dir/EventLoop.o" "gcc" "CMakeFiles/mymuduo.dir/EventLoop.o.d"
+  "/home/djf/sourcecode/mymuduo/EventLoopThread.cc" "CMakeFiles/mymuduo.dir/EventLoopThread.o" "gcc" "CMakeFiles/mymuduo.dir/EventLoopThread.o.d"
+  "/home/djf/sourcecode/mymuduo/EventLoopThreadPool.cc" "CMakeFiles/mymuduo.dir/EventLoopThreadPool.o" "gcc" "CMakeFiles/mymuduo.dir/EventLoopThreadPool.o.d"
+  "/home/djf/sourcecode/mymuduo/InetAddress.cc" "CMakeFiles/mymuduo.dir/InetAddress.o" "gcc" "CMakeFiles/mymuduo.dir/InetAddress.o.d"
+  "/home/djf/sourcecode/mymuduo/Logger.cc" "CMakeFiles/mymuduo.dir/Logger.o" "gcc" "CMakeFiles/mymuduo.dir/Logger.o.d"
+  "/home/djf/sourcecode/mymuduo/Poller.cc" "CMakeFiles/mymuduo.dir/Poller.o" "gcc" "CMakeFiles/mymuduo.dir/Poller.o.d"
+  "/home/djf/sourcecode/mymuduo/Socket.cc" "CMakeFiles/mymuduo.dir/Socket.o" "gcc" "CMakeFiles/mymuduo.dir/Socket.o.d"
+  "/home/djf/sourcecode/mymuduo/TcpConnection.cc" "CMakeFiles/mymuduo.dir/TcpConnection.o" "gcc" "CMakeFiles/mymuduo.dir/TcpConnection.o.d"
+  "/home/djf/sourcecode/mymuduo/TcpServer.cc" "CMakeFiles/mymuduo.dir/TcpServer.o" "gcc" "CMakeFiles/mymuduo.dir/TcpServer.o.d"
+  "/home/djf/sourcecode/mymuduo/Thread.cc" "CMakeFiles/mymuduo.dir/Thread.o" "gcc" "CMakeFiles/mymuduo.dir/Thread.o.d"
+  "/home/djf/sourcecode/mymuduo/Timestamp.cc" "CMakeFiles/mymuduo.dir/Timestamp.o" "gcc" "CMakeFiles/mymuduo.dir/Timestamp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
